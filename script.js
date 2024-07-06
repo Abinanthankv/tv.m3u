be977ee1c0adb245ae883b52118d191b220906e0
Abinanthankv <99575728+Abinanthankv@users.noreply.github.com>
2024-07-06 22:08:29 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,58 @@
+var player = videojs("myVideo",{
+    playbackRates: [0.5, 1, 1.5, 2] // Optional playback rate options
+  });
+  
+  const channelList = document.getElementById("channel-list");
+  const myVideo = document.getElementById("myVideo");
+  const filterSelect = document.getElementById("channel-filter");
+  
+  // Replace 'your_data.json' with the actual path to your JSON file
+  fetch("./channel.json")
+    .then((response) => response.json())
+    .then((data) => {
+      // Extract unique groups from your JSON data
+      const uniqueGroups = [...new Set(data.map((item) => item.group))];
+  
+      // Populate the filter select element with group options
+      uniqueGroups.forEach((group) => {
+        const option = document.createElement("option");
+        option.value = group;
+        option.text = group;
+        filterSelect.appendChild(option);
+      });
+  
+      filterSelect.addEventListener("change", () => {
+        const selectedGroup = filterSelect.value;
+        let filteredData;
+  
+        // Filter data based on selected group
+        if (selectedGroup === "all") {
+          filteredData = data;
+        } else {
+          filteredData = data.filter((item) => item.group === selectedGroup);
+        }
+  
+        // Clear existing channel list and repopulate with filtered data
+        channelList.innerHTML = "";
+        for (const item of filteredData) {
+          const logoImg = document.createElement("img");
+          const listItem = document.createElement("li");
+          logoImg.src = item.logo;
+          listItem.textContent = item.name;
+          listItem.insertAdjacentHTML(
+            "beforeend",
+            `<img src="${item.logo}">`
+          );
+          listItem.addEventListener("click", () => {
+            player.src({
+              src: `https://fifaxbd.fun/JIOxRANAPK/stream.m3u8?id=${item.id}&e=.m3u8`,
+              type: "application/x-mpegURL",
+            });
+            player.play();
+            player.hlsQualitySelector({ displayCurrentQuality: true });
+          });
+          channelList.appendChild(listItem);
+        }
+      });
+    });
+  
