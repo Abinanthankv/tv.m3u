f4ccf1453966c36578ed7bd29c171d37f5a12997
Abinanthankv <99575728+Abinanthankv@users.noreply.github.com>
2024-07-08 00:24:34 +0530

    Update script.js

--- script.js
+++ script.js
@@ -65,7 +65,7 @@ fetch("./channel.json")
    var  currentPlayingPrograminfo="" // Initialize variable to store current program
     const currentPlaying = document.getElementById("current-playing");
     const currentPlayinginfo = document.getElementById("current-playing-info");
-    var currentTime = getTimeshiftedCurrentTime(20500);
+    var currentTime = getTimeshiftedCurrentTime(19800);
     currentPlaying.innerHTML=""
     currentPlayinginfo.innerHTML=""
      //currentPlaying.textContent= "Now Playing "
