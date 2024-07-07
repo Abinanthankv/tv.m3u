2d6af6bb0caf8d9e98de34c8ec347e20557713fc
Abinanthankv <99575728+Abinanthankv@users.noreply.github.com>
2024-07-07 17:09:54 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ var player = videojs("myVideo", {
   playbackRates: [0.5, 1, 1.5, 2], // Optional playback rate options
 });
 player.hlsQualitySelector({ displayCurrentQuality: true });
+
 const channelList = document.getElementById("channel-list");
 const channelName = document.getElementById("channel-name");
 const channelLogo = document.getElementById("channel-logo");
@@ -22,50 +23,29 @@ fetch("./channel.json")
       option.text = group;
       filterSelect.appendChild(option);
     });
-    filterSelect.addEventListener("DOMContentLoaded", loadallchannel());
-
-    function loadallchannel() {
-      // Code to be executed after the DOM is loaded (may not wait for all resources)
-      filterSelect.value = "all";
-      const selectedGroup = filterSelect.value;
-      let filteredData;
-      if (selectedGroup === "all") {
-        filteredData = data;
-      } else {
-        filteredData = data.filter((item) => item.group === selectedGroup);
-      }
-      channelList.innerHTML = "";
-      for (const item of filteredData) {
-        const logoImg = document.createElement("img");
-        const listItem = document.createElement("li");
-        logoImg.src = item.logo;
-        listItem.textContent = item.name;
-        listItem.insertAdjacentHTML("beforeend", `<img src="${item.logo}">`);
-        channelList.appendChild(listItem);
-      }
-    }
-
     filterSelect.addEventListener("change", () => {
       const selectedGroup = filterSelect.value;
       let filteredData;
-
       // Filter data based on selected group
       if (selectedGroup === "all") {
         filteredData = data;
       } else {
         filteredData = data.filter((item) => item.group === selectedGroup);
       }
-
       // Clear existing channel list and repopulate with filtered data
       channelList.innerHTML = "";
       for (const item of filteredData) {
-        const logoImg = document.createElement("img");
+      //  const logoImg = document.createElement("img");
         const listItem = document.createElement("li");
-        logoImg.src = item.logo;
+       // logoImg.src = item.logo;
         listItem.textContent = item.name;
         listItem.insertAdjacentHTML("beforeend", `<img src="${item.logo}">`);
         listItem.addEventListener("click", () => {
-          var channelData = getChannelDataById(item.id);
+          
+           var epgid=generateEpgId(item.name);
+           console.log(epgid);
+           var channelData = getChannelDataById(epgid);
+
           channelName.textContent = item.name;
           channelLogo.src = item.logo;
           player.pause();
@@ -80,50 +60,67 @@ fetch("./channel.json")
       }
     });
   });
-function getChannelDataById(channelId) {
-  fetch("./epg.xml")
-    .then((response) => response.text())
-    .then((xmlData) => {
-      // Parse the XML data using DOMParser (as shown in previous example)
-      const parser = new DOMParser();
-      const xmlDoc = parser.parseFromString(xmlData, "text/xml");
-      var channels = xmlDoc.getElementsByTagName("programme");
-      for (var i = 0; i < channels.length; i++) {
-        var channel = channels[i];
-        var isPlaying = isProgramPlaying(channel);
-
-        var currentChannelId = channel.getAttribute("channel");
-        if (currentChannelId === channelId) {
-          // Extract relevant data from the channel element
-          var displayName =
-            channel.getElementsByTagName("title")[0].textContent;
-          var iconSrc = channel
-            .getElementsByTagName("icon")[0]
-            .getAttribute("src");
-          // if (isPlaying) {
-          //   console.log("Program", channel.getElementsByTagName("title")[0].textContent, "is currently playing");
-          // } else {
-          //   console.log("Program is not currently playing");
-          // }
-          // Return an object containing the extracted data
+  function getChannelDataById(channelId) {
+    var currentPlayingProgram=""; // Initialize variable to store current program
+    const currentPlaying = document.getElementById("current-playing");
+     currentPlaying.textContent= "Now Playing "
+    fetch("./epg_ripper_IN1.xml")
+      .then((response) => response.text())
+      .then((xmlData) => {
+        const parser = new DOMParser();
+        const xmlDoc = parser.parseFromString(xmlData, "text/xml");
+        var channels = xmlDoc.getElementsByTagName("programme");
+  
+        for (var i = 0; i < channels.length; i++) {
+          var channel = channels[i];
+          var currentChannelId = channel.getAttribute("channel").toUpperCase();
+          
+  
+          if (currentChannelId === channelId) {
+            console.log("cuurentchannel",currentChannelId)
+            // Extract program data
+            var programStart = parseInt(channel.getAttribute("start"));
+            var programStop = parseInt(channel.getAttribute("stop"));
+            var currentTime = getFormattedCurrentTime();
+  
+            // Check if program is currently playing
+            if (programStart < currentTime && currentTime < programStop) {
+              var title = channel.getElementsByTagName("title")[0].textContent;
+              currentPlayingProgram = title; // Store the title of the current program
+              console.log(currentPlayingProgram)
+              currentPlaying.textContent= "Now Playing "+ currentPlayingProgram;
+              break; // Stop iterating after finding the current program
+            } else {
+              currentPlayingProgram = "No data available"; // Default value if not playing
+              currentPlaying.textContent= "Now Playing "+ currentPlayingProgram;
+            }
+          }
         }
-      }
-
-      // Channel ID not found, return null
-      return null;
-    });
-}
+  
+        // Return the current playing program title or "No data available"
+       currentPlaying.textContent= "Now Playing "+ currentPlayingProgram;
+        return currentPlayingProgram;
+      })
+      
+  }
+  function generateEpgId(channelName) {
+    // Remove unnecessary characters and spaces for a cleaner format
+    const sanitizedName = channelName.replace(/\W/g, '.').toUpperCase();
+    return `${sanitizedName}.IN`;
+  }
 function isProgramPlaying(programmeData) {
-  var currentTime = getFormattedCurrentTime();
-  //var currentMillis = currentTime.getTime();
+  // This function is not used directly in the corrected code,
+  // but it's included for reference. It can be used for debugging or
+  // additional processing if needed.
 
+  var currentTime = getFormattedCurrentTime();
   var programStart = parseInt(programmeData.getAttribute("start"));
   var programStop = parseInt(programmeData.getAttribute("stop"));
+
   if (programStart < currentTime && currentTime < programStop) {
-    var title = programmeData.getElementsByTagName("title")[0].textContent;
-    console.log(title);
+    return programmeData.getElementsByTagName("title")[0].textContent; // Return program title
   } else {
-    console.log("No data available");
+    return "No data available"; // Return default value
   }
 }
 function getFormattedCurrentTime() {
@@ -136,7 +133,7 @@ function getFormattedCurrentTime() {
   var second = now.getSeconds().toString().padStart(2, "0");
 
   // Combine the formatted components into the desired format
-  var formattedTime = year + month + day + hour + minute + second;
+  var formattedTime = year + month + day + hour + minute+"00";
   return formattedTime;
 }
 
