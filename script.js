494842737f8f17209963d3be1ce6a0bfdc125360
Abinanthankv <99575728+Abinanthankv@users.noreply.github.com>
2024-07-08 19:50:51 +0530

    Update script.js

--- script.js
+++ script.js
@@ -7,31 +7,73 @@ const channelList = document.getElementById("channel-list");
 const channelName = document.getElementById("channel-name");
 const channelLogo = document.getElementById("channel-logo");
 const myVideo = document.getElementById("myVideo");
-const filterSelect = document.getElementById("channel-filter");
+const filterLanguage = document.getElementById("language-filter");
+const filterCategory = document.getElementById("channel-filter");
 
 // Replace 'your_data.json' with the actual path to your JSON file
 fetch("./streams.json")
   .then((response) => response.json())
   .then((data) => {
-    // Extract unique groups from your JSON data
-    const uniqueGroups = [...new Set(data.map((item) => item.category))];
+    // Extract unique groups (languages) and categories from your JSON data
+    const uniqueGroups = [...new Set(data.map((item) => item.language))];
+    const uniqueCategories = [...new Set(data.map((item) => item.category))];
 
-    // Populate the filter select element with group options
-    uniqueGroups.forEach((group) => {
+    // Populate the filter select element with group (language) and category options
+    uniqueGroups.forEach((language) => {
       const option = document.createElement("option");
-      option.value = group;
-      option.text = group;
-      filterSelect.appendChild(option);
+      option.value = language;
+      option.text = language;
+      filterLanguage.appendChild(option);
     });
-    filterSelect.addEventListener("change", () => {
-      const selectedGroup = filterSelect.value;
+
+    uniqueCategories.forEach((category) => {
+      const option = document.createElement("option");
+      option.value = category;
+      option.text = category;
+      filterCategory.appendChild(option);
+    });
+    filterCategory.parentElement.appendChild(filterLanguage);
+    filterCategory.addEventListener("change", () => {
+     channelList.innerHTML = "";
+      const selectedGroup = filterLanguage.value;
+      const selectedCategory = filterCategory.value;
       let filteredData;
-      // Filter data based on selected group
-      if (selectedGroup === "all") {
+      console.log("selectedCategory",selectedCategory);
+      console.log("selectedGroup",selectedGroup);
+      if (selectedGroup === "all" && selectedCategory === "all") {
         filteredData = data;
+      } else if (selectedGroup === "all") {
+        filteredData = data.filter((item) => item.category === selectedCategory);
+      } else if (selectedCategory === "all") {
+        filteredData = data.filter((item) => item.language === selectedGroup);
       } else {
-        filteredData = data.filter((item) => item.category === selectedGroup);
+        filteredData = data.filter(
+          (item) => item.language === selectedGroup && item.category === selectedCategory
+        );
       }
+      console.log("filtereddata",filteredData)
+      filterLanguage.value="all"
+      filterCategory.value=selectedCategory;
+      console.log(filterCategory)
+    
+    });
+    filterLanguage.addEventListener("change", () => {
+      const selectedGroup = filterLanguage.value;
+      const selectedCategory = filterCategory.value;
+      let filteredData;
+      // Filter data based on selected group (language) and category
+      if (selectedGroup === "all" && selectedCategory === "all") {
+        filteredData = data;
+      } else if (selectedGroup === "all") {
+        filteredData = data.filter((item) => item.category === selectedCategory);
+      } else if (selectedCategory === "all") {
+        filteredData = data.filter((item) => item.language === selectedGroup);
+      } else {
+        filteredData = data.filter(
+          (item) => item.language === selectedGroup && item.category === selectedCategory
+        );
+      }
+    
       // Clear existing channel list and repopulate with filtered data
       channelList.innerHTML = "";
       for (const item of filteredData) {
@@ -56,7 +98,6 @@ fetch("./streams.json")
             player.play();
           }
           else{
-           
             console.log(sonyid);
            player.pause();
            player.on('error', function(error) {
